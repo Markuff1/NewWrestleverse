6722dfae3c8d3f58e60c7fe3fc4865a340a1396c
Markuff1 <marknixon1@outlook.com>
2026-04-20 13:42:20 +0100

    NOC Data

--- src/currentPPV.ts
+++ src/currentPPV.ts
@@ -1,9 +1,9 @@
 // currentPPV.ts
 
 export const currentPPV = {
-  name: "Summerslam",
-  location: "Metlife Stadium, New York, NJ",
-  date: "Saturday, August 7th, 2027",
-  image: "/Images/PPV/SummerSlam/SS2027.png",
-  link: "/SummerSlam"
+  name: "Night Of Champions",
+  location: "Kingdom Arena, Riyadh, Saudi Arabia",
+  date: "Saturday, September 25th 2027, 2pm ET / 11am PT",
+  image: "/Images/PPV/NOC/NOC2027.png",
+  link: "/NOC"
 };
